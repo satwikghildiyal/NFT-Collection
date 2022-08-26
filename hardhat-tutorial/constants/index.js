7f6108317372d02a03a634d6931920272821a941
Satwik Ghildiyal <satwikghildiyal15112001@gmail.com>
2022-08-26 18:38:58 +0530

    intial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+
+const WHITELIST_CONTRACT_ADDRESS = "0x68A7d9EfB765E9aFB7021D3799a2116ff4Ff5b84";
+
+const METADATA_URL = "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
